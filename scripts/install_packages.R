6369022efa7f3d1651c67095d6a661a1861432df
Andy Lyons <lyons.andy@gmail.com>
2024-05-02 10:10:31 -0700

    Update install_packages.R

--- scripts/install_packages.R
+++ scripts/install_packages.R
@@ -1,11 +1,10 @@
 #########################################################################################
 ## GENERAL TIPS FOR INSTALLING PACKAGES
 ##
-## If you are prompted by the question, "Do you want to install from sources the 
-## package which needs compilation?", choose "No".
+## Restarting R (Session > Restart R) is always a good idea before you install or update packages
 ##
-## If you get an error message that a package can't be installed because it's already 
-## loaded and can't be stopped, restart R (Session >> Restart R), and try again.
+## If you are prompted by the question, "Do you want to install from sources the package 
+## which needs compilation?", choose "No".
 ##
 ## If you are working in RStudio Desktop (as opposed to Posit Cloud), running these commands 
 ## in R (rather than RStudio) can help resolve issues.
@@ -13,19 +12,22 @@
 
 ## Required packages from CRAN
 
-pkgs_req <- c("sf", "tidyverse", "conflicted", "here", "units", "zoo", "degday", "cimir", "httr2", "jsonlite", "usethis", 
-              "shiny", "leaflet", "glue", "DT", "plotly", "openmeteo", "caladaptr")
+cran_pkgs <- c("sf", "tidyverse", "conflicted", "here", "units", "zoo", "degday", "cimir", "httr2", "jsonlite", "usethis", 
+              "shiny", "leaflet", "glue", "DT", "plotly", "openmeteo")
 
-install.packages(pkgs_req, repos = c("https://cloud.r-project.org", "https://ajlyons.r-universe.dev"))
+install.packages(cran_pkgs)
+
+missing_pkgs <- cran_pkgs[!(cran_pkgs %in% installed.packages()[,"Package"])]
 
 ## Was it successful?
 
-pkgs_missing <- pkgs_req[!(pkgs_req %in% installed.packages()[,"Package"])]
-
-if (length(pkgs_missing)==0) {
-  cat("ALL REQUIRED PACKAGES ARE INSTALLED :-) \n")
+if (length(missing_pkgs)==0) {
+  cat("ALL REQUIRED CRAN PACKAGES ARE INSTALLED :-) \n")
 } else {
-  cat("ONE OR MORE PACKAGES ARE NOT INSTALLED :-(\n", paste(pkgs_missing, collapse = ", "), "\n")
+  cat("ONE OR MORE PACKAGES ARE NOT INSTALLED :-(\n", paste(missing_pkgs, collapse = ", "), "\n")
 }
 
+## Restart R (Session > Restart R), then install caladaptr from R-Universe
+
+install.packages("caladaptr", repos = "https://ajlyons.r-universe.dev")
 
